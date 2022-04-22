a1c3bb86a8ca757580706788e6b9924e44017cfd
Facundo Leañez <facundo.leanez@hotmail.com>
2022-04-22 15:26:36 +1000

    Initial Commit

--- App.tsx
+++ App.tsx
@@ -1,14 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { Provider as PaperProvider } from 'react-native-paper'
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +9,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </View>
+    </PaperProvider>
+  )
+}
